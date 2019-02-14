cb7feb29b7b45dc79af636ed4c90af48320cfc64
Vincent Biret <vincentbiret@hotmail.com>
2019-02-14 16:12:06 -0500

    fixed missing removal

--- samples/azure-devops-ci-cd-spfx/gulpfile.js
+++ samples/azure-devops-ci-cd-spfx/gulpfile.js
@@ -3,7 +3,6 @@
 const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
-const _ = require('lodash');
 
 build.initialize(gulp);
 var buildConfig = build.getConfig();
