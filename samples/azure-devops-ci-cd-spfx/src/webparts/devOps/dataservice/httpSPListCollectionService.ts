1ee8a6bcfe0da66a0d22ed674834f834b2a91f78
Vincent Biret <vincentbiret@hotmail.com>
2019-12-01 15:54:33 -0500

    - removes knockout webpart

--- samples/azure-devops-ci-cd-spfx/src/webparts/devOps/dataservice/httpSPListCollectionService.ts
+++
@@ -1,8 +0,0 @@
-import { ISPListCollectionService, ISPList } from "./";
-import { sp } from "@pnp/sp";
-
-export class HttpSPListCollectionService implements ISPListCollectionService {
-    public async getListData(): Promise<ISPList[]> {
-        return (await sp.web.lists.filter(`Hidden eq false`).get()) as ISPList[];
-    }
-}
